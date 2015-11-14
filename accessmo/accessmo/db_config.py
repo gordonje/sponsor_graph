b302c12e53dade4f22ff1a18b9ea5139daea0450
James Gordon <gordon.je@gmail.com>
2015-11-13 21:29:25 -0600

    Init commit

---
+++ accessmo/accessmo/db_config.py
@@ -0,0 +1,10 @@
+postgresdb = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'accessmo',
+        'USER': 'accessmo',
+        'PASSWORD': 'eZ2Y6NR5TZ5MbYw',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
