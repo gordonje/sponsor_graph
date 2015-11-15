76bca31e805ce40de35ec16579f7a215a9b9617a
Nathan Lawrence <nathan@nathanlawrence.org>
2015-11-14 19:52:13 -0600

    Have yourself a somewhat finished product.

--- static/js/graph-make.js
+++ static/js/graph-make.js
@@ -1,69 +1,7 @@
 var margin = {top: 20, right: 20, bottom: 60, left: 50},
-    width = 800 - margin.left - margin.right,
+    width = 1200 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
 
-var people = [
-  { 'name':'John', 
-    'index':0,
-    'party':'Republican'
-  },
-    { 'name':'Kenneth', 
-    'index':1,
-    'party':'Democrat'
-  },
-  { 'name':'Mary', 
-    'index':3,
-    'party':'Republican'
-  }
-];
-
-var links = [
-  {
-    'source':0,
-    'target':1,
-    'shared_vote_count' :12
-  },
-  {
-    'source':1,
-    'target':2,
-    'shared_vote_count' :20
-  },
-  {
-    'source':0,
-    'target':2,
-    'shared_vote_count' :30
-  },
-  {
-    'source':1,
-    'target':0,
-    'shared_vote_count' :2
-  },
-  {
-    'source':2,
-    'target':1,
-    'shared_vote_count' :2
-  },
-  {
-    'source':0,
-    'target':0,
-    'shared_vote_count' :2
-  },
-  {
-    'source':1,
-    'target':1,
-    'shared_vote_count' :22
-  },
-  {
-    'source':2,
-    'target':2,
-    'shared_vote_count' :27
-  },
-  {
-    'source':2,
-    'target':0,
-    'shared_vote_count' :16
-  }
-]
 
 var cellWidth = width / people.length;
 var cellHeight = height / people.length;
@@ -85,7 +23,7 @@ for (var i=0; i < links.length; i++){
 for (var i=0; i < links.length; i++){
   //Basic selection of "link"
   var link = links[i];
-  console.log(link);
+  // console.log(link);
 
   //Making the Rectangle itself
   var rect = svg.append("rect")
@@ -99,7 +37,7 @@ for (var i=0; i < links.length; i++){
   rect.attr("y",gridVertPosition);
 
   //Alt Text
-  rect.attr("data-hover-text", people[link.source].name + " (" + people[link.source].party + ")" + " & " + people[link.target].name + " (" + people[link.target].party + ")");
+  rect.attr("data-hover-text", people[link.source].name + " (" + people[link.source].party + ")" + " & " + people[link.target].name + " (" + people[link.target].party + ") share " + link.shared_vote_count + " votes.");
 
   //Give the rect jQuery-targetable class
   rect.classed("grid-rect",true);
@@ -107,15 +45,22 @@ for (var i=0; i < links.length; i++){
 
   //Giving the Rectangle Color
   var colorBefore = "rgba(200, 200, 200, " + link.shared_vote_count/maxRelationship + ")";
-  if (people[link.source].party == "Republican"){
-    if (people[link.target].party == "Republican"){
+  if (people[link.source].party == "Republican" || people[link.source].party == "Libertarian"){
+    if (people[link.target].party == "Republican" || people[link.source].party == "Libertarian"){
         colorBefore = "rgba(128, 0, 0, " + link.shared_vote_count/maxRelationship + ")";
     }
+    else{
+      colorBefore = "rgba(128, 0, 128, " + link.shared_vote_count/maxRelationship + ")";
+    }
   }
-  else if (people[link.source].party == "Democrat"){
-    if (people[link.target].party == "Democrat"){
+  else if (people[link.source].party == "Democratic"){
+    if (people[link.target].party == "Democratic"){
        colorBefore = "rgba(0, 51, 153," + link.shared_vote_count/maxRelationship + ")";
     }
+    else{
+      colorBefore = "rgba(128, 0, 128, " + link.shared_vote_count/maxRelationship + ")";
+    }
+
   }
   rect.style("fill",colorBefore);
   var colorAfter = "#f5f5f5";
